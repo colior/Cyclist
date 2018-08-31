edc8f863f1c7df4fcabfabe91c664e54efa50e51
Ben <bencohen3@gmail.com>
2018-08-31 13:38:28 +0300

    install osmdroid and osmbonuspack libraries and all other dependencies. add simple mapview to mainactivity.

--- app/src/main/java/com/cyclist/MainActivity.java
+++ app/src/main/java/com/cyclist/MainActivity.java
@@ -1,13 +1,44 @@
 package com.cyclist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MapView map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //handle permissions first, before map is created. not depicted here
+
+        //load/initialize the osmdroid configuration, this can be done
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
+        //inflate and create the map
+        setContentView(R.layout.activity_main);
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        GeoPoint startPoint = new GeoPoint(48.13, -1.63);
+        IMapController mapController = map.getController();
+        //mapController.setZoom(9);
+        mapController.setCenter(startPoint);
     }
 }
