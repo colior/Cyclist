e4010585d0d55640e446dee14462e893ad92cf09
EDENHARTMAN <rose.hartman@hotmail.com>
2018-09-18 11:55:10 +0300

    add auto complete convert address to coordinates

--- app/src/main/java/com/cyclist/logic/search/SearchService.java
+++ app/src/main/java/com/cyclist/logic/search/SearchService.java
@@ -1,5 +1,30 @@
 package com.cyclist.logic.search;
 
+import android.location.Address;
+import android.location.Geocoder;
+
+import com.cyclist.activities.MainActivity;
+
+import org.osmdroid.util.GeoPoint;
+
+import java.io.IOException;
+import java.util.List;
+
 public class SearchService {
+
+    public GeoPoint convertAddressToGeopoint(String addressStr, Geocoder geocoder) {
+        GeoPoint addressGeoPoint = null;
+        if ((addressStr != null) && !(addressStr.equals(""))) {
+            List<Address> addressList = null;
+            try {
+                addressList = geocoder.getFromLocationName(addressStr, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            addressGeoPoint = new GeoPoint(address.getLatitude(), address.getLongitude());
+        }
+        return addressGeoPoint;
+    }
 
 }
